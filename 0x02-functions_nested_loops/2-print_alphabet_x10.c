84dc682822412dfd148796a4388866baf0494bb2
menna <menna.jaheen16@gmail.com>
2023-03-21 18:55:10 -0700

    crying

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * Print_alphapet_x10 - function that will print the alphapet 10 times
+ *
+*/
+
+void print_alphapet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+}
